a59be80497d654e10c4aa773bf3518c01bcfdb24
cephalopagus <dastan.bak@gmail.com>
2025-12-13 01:46:33 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var (
+		name   = "Дастан"
+		age    = 23
+		height = 180
+	)
+	fmt.Println("Имя:", name, "\nВозраст:", age, "\nРост:", height)
+}
