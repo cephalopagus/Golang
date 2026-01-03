2d03885f13a7db21e544fd90d144d0d4177df5dd
cephalopagus <dastan.bak@gmail.com>
2026-01-04 00:58:34 +0600

    f

--- MyErr/main.go
+++ MyErr/main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	check := 0
-	num, err := 2 / check
-	if err != nil {
-		fmt.Errorf("math err - %w", err)
-	} else {
-		fmt.Println(num)
-	}
+	// num, err := 2 // check
+	// if err != nil {
+	// 	fmt.Errorf("math err - %w", err)
+	// } else {
+	// 	fmt.Println(num)
+	// }
 }
